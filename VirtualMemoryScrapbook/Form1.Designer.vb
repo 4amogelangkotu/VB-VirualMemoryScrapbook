5be6f275fdddf463bfbc5a0b79bb8ea7a15a7be8
4amogelangkotu <4kotuamogelang@gmail.com>
2025-01-20 23:35:17 +0200

    initial commit of the files

---
+++ VirtualMemoryScrapbook/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VMS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pbPhoto = New System.Windows.Forms.PictureBox()
+        Me.txtCaption = New System.Windows.Forms.TextBox()
+        Me.lstPhotos = New System.Windows.Forms.ListBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnAddPhoto = New System.Windows.Forms.Button()
+        Me.btnDelPhoto = New System.Windows.Forms.Button()
+        Me.ofdPhoto = New System.Windows.Forms.OpenFileDialog()
+        Me.cmbThemes = New System.Windows.Forms.ComboBox()
+        Me.btnSlideshow = New System.Windows.Forms.Button()
+        Me.slideshowTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.btnPDF = New System.Windows.Forms.Button()
+        Me.btnCaption = New System.Windows.Forms.Button()
+        CType(Me.pbPhoto, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbPhoto
+        '
+        Me.pbPhoto.Location = New System.Drawing.Point(12, 12)
+        Me.pbPhoto.Name = "pbPhoto"
+        Me.pbPhoto.Size = New System.Drawing.Size(300, 300)
+        Me.pbPhoto.TabIndex = 0
+        Me.pbPhoto.TabStop = False
+        '
+        'txtCaption
+        '
+        Me.txtCaption.Location = New System.Drawing.Point(12, 318)
+        Me.txtCaption.Multiline = True
+        Me.txtCaption.Name = "txtCaption"
+        Me.txtCaption.ReadOnly = True
+        Me.txtCaption.Size = New System.Drawing.Size(300, 26)
+        Me.txtCaption.TabIndex = 1
+        '
+        'lstPhotos
+        '
+        Me.lstPhotos.FormattingEnabled = True
+        Me.lstPhotos.ItemHeight = 20
+        Me.lstPhotos.Location = New System.Drawing.Point(361, 46)
+        Me.lstPhotos.Name = "lstPhotos"
+        Me.lstPhotos.Size = New System.Drawing.Size(189, 184)
+        Me.lstPhotos.TabIndex = 2
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(361, 247)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(189, 38)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Save Scrapbook"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(361, 291)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(189, 38)
+        Me.btnLoad.TabIndex = 4
+        Me.btnLoad.Text = "Load Scrapbook"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnAddPhoto
+        '
+        Me.btnAddPhoto.Location = New System.Drawing.Point(12, 351)
+        Me.btnAddPhoto.Name = "btnAddPhoto"
+        Me.btnAddPhoto.Size = New System.Drawing.Size(145, 30)
+        Me.btnAddPhoto.TabIndex = 5
+        Me.btnAddPhoto.Text = "Add Photo"
+        Me.btnAddPhoto.UseVisualStyleBackColor = True
+        '
+        'btnDelPhoto
+        '
+        Me.btnDelPhoto.Location = New System.Drawing.Point(167, 351)
+        Me.btnDelPhoto.Name = "btnDelPhoto"
+        Me.btnDelPhoto.Size = New System.Drawing.Size(145, 30)
+        Me.btnDelPhoto.TabIndex = 6
+        Me.btnDelPhoto.Text = "Delete Photo"
+        Me.btnDelPhoto.UseVisualStyleBackColor = True
+        '
+        'ofdPhoto
+        '
+        Me.ofdPhoto.FileName = "odfPhoto"
+        '
+        'cmbThemes
+        '
+        Me.cmbThemes.FormattingEnabled = True
+        Me.cmbThemes.Location = New System.Drawing.Point(361, 12)
+        Me.cmbThemes.Name = "cmbThemes"
+        Me.cmbThemes.Size = New System.Drawing.Size(189, 28)
+        Me.cmbThemes.TabIndex = 8
+        Me.cmbThemes.Text = "change theme"
+        '
+        'btnSlideshow
+        '
+        Me.btnSlideshow.Location = New System.Drawing.Point(361, 335)
+        Me.btnSlideshow.Name = "btnSlideshow"
+        Me.btnSlideshow.Size = New System.Drawing.Size(189, 38)
+        Me.btnSlideshow.TabIndex = 9
+        Me.btnSlideshow.Text = "Start Slideshow"
+        Me.btnSlideshow.UseVisualStyleBackColor = True
+        '
+        'slideshowTimer
+        '
+        Me.slideshowTimer.Interval = 2000
+        '
+        'btnPDF
+        '
+        Me.btnPDF.Location = New System.Drawing.Point(361, 379)
+        Me.btnPDF.Name = "btnPDF"
+        Me.btnPDF.Size = New System.Drawing.Size(189, 38)
+        Me.btnPDF.TabIndex = 10
+        Me.btnPDF.Text = "Export to PDF"
+        Me.btnPDF.UseVisualStyleBackColor = True
+        '
+        'btnCaption
+        '
+        Me.btnCaption.Location = New System.Drawing.Point(12, 387)
+        Me.btnCaption.Name = "btnCaption"
+        Me.btnCaption.Size = New System.Drawing.Size(300, 30)
+        Me.btnCaption.TabIndex = 11
+        Me.btnCaption.Text = "Update Caption"
+        Me.btnCaption.UseVisualStyleBackColor = True
+        '
+        'VMS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 429)
+        Me.Controls.Add(Me.btnCaption)
+        Me.Controls.Add(Me.btnPDF)
+        Me.Controls.Add(Me.btnSlideshow)
+        Me.Controls.Add(Me.cmbThemes)
+        Me.Controls.Add(Me.btnDelPhoto)
+        Me.Controls.Add(Me.btnAddPhoto)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.lstPhotos)
+        Me.Controls.Add(Me.txtCaption)
+        Me.Controls.Add(Me.pbPhoto)
+        Me.Name = "VMS"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Text = "Virtual Memory Scrapbook"
+        CType(Me.pbPhoto, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pbPhoto As PictureBox
+    Friend WithEvents txtCaption As TextBox
+    Friend WithEvents lstPhotos As ListBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnAddPhoto As Button
+    Friend WithEvents btnDelPhoto As Button
+    Friend WithEvents ofdPhoto As OpenFileDialog
+    Friend WithEvents cmbThemes As ComboBox
+    Friend WithEvents btnSlideshow As Button
+    Friend WithEvents slideshowTimer As Timer
+    Friend WithEvents btnPDF As Button
+    Friend WithEvents btnCaption As Button
+End Class
---
+++ VirtualMemoryScrapbook/Form1.vb
@@ -0,0 +1,256 @@
+﻿Imports System.IO
+Imports System.Drawing
+Imports System.Collections.Generic
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+
+Public Class VMS
+    'variables
+    Private photos As New List(Of String)()
+    Private captions As New List(Of String)()
+    Private currentSlideIndex As Integer = 0
+    Private photoCaptions As New Dictionary(Of String, String)
+
+    Private Sub VMS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbThemes.Items.AddRange(New String() {"Light Theme", "Dark Theme", "Custom Color"})
+        cmbThemes.SelectedIndex = 0
+    End Sub
+
+    Private Sub btnAddPhoto_Click(sender As Object, e As EventArgs) Handles btnAddPhoto.Click
+        ' Create an OpenFileDialog to select an image
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif"
+        openFileDialog.Title = "Select a Photo to Add"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = openFileDialog.FileName
+
+            Try
+                ' Load the image into the PictureBox
+                pbPhoto.Image = Drawing.Image.FromFile(filePath)
+                pbPhoto.SizeMode = PictureBoxSizeMode.Zoom
+
+                ' Prompt for a caption
+                Dim caption As String = InputBox("Enter a caption for this photo:", "Add Caption")
+
+                ' Add the file path and caption to the dictionary
+                photoCaptions(filePath) = caption
+
+                ' Update the ListBox to show captions
+                lstPhotos.Items.Add($"{Path.GetFileName(filePath)}")
+
+                photos.Add(filePath)
+                captions.Add(caption)
+
+                txtCaption.Text = caption
+            Catch ex As Exception
+                MessageBox.Show("An error occurred while loading the image: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnCaption_Click(sender As Object, e As EventArgs) Handles btnCaption.Click
+        Dim index As Integer = lstPhotos.SelectedIndex
+        If index >= 0 Then
+            Dim selectedFilePath As String = photos(index)
+
+            ' Ensure the selected file path is valid
+            If Not String.IsNullOrEmpty(selectedFilePath) AndAlso File.Exists(selectedFilePath) Then
+                ' Get the current caption or default to an empty string if not found
+                Dim currentCaption As String = If(photoCaptions.ContainsKey(selectedFilePath), photoCaptions(selectedFilePath), "")
+                Dim newCaption As String = InputBox("Enter a new caption for this photo:", "Update Caption", currentCaption)
+
+                ' Update the caption if a new value is provided
+                If Not String.IsNullOrEmpty(newCaption) Then
+                    photoCaptions(selectedFilePath) = newCaption
+                    captions(index) = newCaption
+                    txtCaption.Text = newCaption
+                End If
+            Else
+                MessageBox.Show("The selected file path is invalid or does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            MessageBox.Show("No photo is selected. Please select a photo first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub lstPhotos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstPhotos.SelectedIndexChanged
+        ' Display the selected photo and its caption
+        Dim index As Integer = lstPhotos.SelectedIndex
+        If index >= 0 Then
+            Dim selectedFilePath As String = photos(index)
+
+            ' Check if the file path is valid and exists
+            If Not String.IsNullOrEmpty(selectedFilePath) AndAlso File.Exists(selectedFilePath) Then
+                pbPhoto.Image = Drawing.Image.FromFile(selectedFilePath)
+                pbPhoto.SizeMode = PictureBoxSizeMode.Zoom
+                txtCaption.Text = captions(index)
+            Else
+                MessageBox.Show($"The file at '{selectedFilePath}' could not be found or is invalid.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                pbPhoto.Image = Nothing
+                txtCaption.Clear()
+            End If
+        End If
+    End Sub
+
+    Private Sub btnDelPhoto_Click(sender As Object, e As EventArgs) Handles btnDelPhoto.Click
+        Dim index As Integer = lstPhotos.SelectedIndex
+        If index >= 0 Then
+            photos.RemoveAt(index)
+            captions.RemoveAt(index)
+            lstPhotos.Items.RemoveAt(index)
+
+            ' Check if there are photos left in the list
+            If lstPhotos.Items.Count > 0 Then
+                ' Select the first photo in the list
+                lstPhotos.SelectedIndex = 0
+            Else
+                ' Clear the picture box and caption if no photos are left
+                pbPhoto.Image = Nothing
+                txtCaption.Clear()
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Using saveFile As New SaveFileDialog()
+            saveFile.Filter = "Scrapbook Files (*.sbk)|*.sbk"
+            If saveFile.ShowDialog() = DialogResult.OK Then
+                Dim sbkFilePath = saveFile.FileName
+                Dim sbkDirectory = Path.GetDirectoryName(sbkFilePath)
+                Dim photosFolder = Path.Combine(sbkDirectory, "photos")
+
+                ' Create the photos folder if it doesn't exist
+                If Not Directory.Exists(photosFolder) Then
+                    Directory.CreateDirectory(photosFolder)
+                End If
+
+                ' Save the scrapbook file
+                Using writer As New StreamWriter(sbkFilePath)
+                    For i = 0 To photos.Count - 1
+                        ' Copy photo to the "photos" folder
+                        Dim photoFileName = Path.GetFileName(photos(i))
+                        Dim destinationPath = Path.Combine(photosFolder, photoFileName)
+
+                        ' Ensure the file isn't overwritten unnecessarily
+                        If Not File.Exists(destinationPath) Then
+                            File.Copy(photos(i), destinationPath)
+                        End If
+
+                        ' Write the relative path and caption to the scrapbook file
+                        writer.WriteLine($"photos\{photoFileName}|{captions(i)}")
+                    Next
+                End Using
+
+                MessageBox.Show("Scrapbook saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End Using
+    End Sub
+
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Using openFile As New OpenFileDialog()
+            openFile.Filter = "Scrapbook Files (*.sbk)|*.sbk"
+            If openFile.ShowDialog() = DialogResult.OK Then
+                Dim sbkFilePath = openFile.FileName
+                Dim sbkDirectory = Path.GetDirectoryName(sbkFilePath)
+
+                photos.Clear()
+                captions.Clear()
+                lstPhotos.Items.Clear()
+
+                ' Read the scrapbook file
+                Using reader As New StreamReader(sbkFilePath)
+                    While Not reader.EndOfStream
+                        Dim line = reader.ReadLine()
+                        Dim parts = line.Split("|"c)
+
+                        ' Construct full photo path from the relative path
+                        Dim relativePhotoPath = parts(0)
+                        Dim fullPhotoPath = Path.Combine(sbkDirectory, relativePhotoPath)
+
+                        If File.Exists(fullPhotoPath) Then
+                            photos.Add(fullPhotoPath)
+                            captions.Add(parts(1))
+                            lstPhotos.Items.Add(Path.GetFileName(fullPhotoPath))
+                        Else
+                            MessageBox.Show($"Photo '{relativePhotoPath}' not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                        End If
+                    End While
+                End Using
+
+                ' Select the first photo if available
+                If photos.Count > 0 Then
+                    lstPhotos.SelectedIndex = 0
+                End If
+
+                MessageBox.Show("Scrapbook loaded successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End Using
+    End Sub
+
+
+    Private Sub cmbThemes_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbThemes.SelectedIndexChanged
+        Select Case cmbThemes.SelectedItem.ToString()
+            Case "Light Theme"
+                Me.BackColor = Color.White
+                lstPhotos.BackColor = Color.LightGray
+            Case "Dark Theme"
+                Me.BackColor = Color.Black
+                lstPhotos.BackColor = Color.DarkGray
+                lstPhotos.ForeColor = Color.White
+            Case "Custom Color"
+                Using colorDialog As New ColorDialog()
+                    If colorDialog.ShowDialog() = DialogResult.OK Then
+                        Me.BackColor = colorDialog.Color
+                    End If
+                End Using
+        End Select
+    End Sub
+
+    Private Sub btnSlideshow_Click(sender As Object, e As EventArgs) Handles btnSlideshow.Click
+        If photos.Count > 0 Then
+            currentSlideIndex = 0
+            slideshowTimer.Start()
+        Else
+            MessageBox.Show("No photos available for slideshow.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub slideshowTimer_Tick(sender As Object, e As EventArgs) Handles slideshowTimer.Tick
+        If currentSlideIndex < photos.Count Then
+            pbPhoto.Image = Drawing.Image.FromFile(photos(currentSlideIndex))
+            txtCaption.Text = captions(currentSlideIndex)
+            currentSlideIndex += 1
+        Else
+            slideshowTimer.Stop()
+            MessageBox.Show("Slideshow finished.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub btnPDF_Click(sender As Object, e As EventArgs) Handles btnPDF.Click
+        Using saveFile As New SaveFileDialog()
+            saveFile.Filter = "PDF Files (*.pdf)|*.pdf"
+            If saveFile.ShowDialog() = DialogResult.OK Then
+                Dim doc As New Document()
+                PdfWriter.GetInstance(doc, New FileStream(saveFile.FileName, FileMode.Create))
+                doc.Open()
+
+                For i = 0 To photos.Count - 1
+                    ' Add image
+                    Dim img As iTextSharp.text.Image = iTextSharp.text.Image.GetInstance(photos(i))
+                    img.ScaleToFit(500.0F, 500.0F)
+                    doc.Add(img)
+
+                    ' Add caption
+                    doc.Add(New Paragraph(captions(i)) With {.Alignment = Element.ALIGN_CENTER})
+                    doc.Add(New Paragraph(" ")) ' Add spacing
+                Next
+
+                doc.Close()
+                MessageBox.Show("PDF Exported Successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End Using
+
+    End Sub
+End Class
